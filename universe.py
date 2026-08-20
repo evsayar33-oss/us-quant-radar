ebf51b1c1c36709622e92a79076718ea341c8a6f
evsayar33-oss <evsayar33@gmail.com>
2026-08-20 22:48:39 +0300

    universe.py

--- universe.py
+++ universe.py
@@ -1,22 +1,17 @@
 import pandas as pd
+import requests
 
 def get_universe():
+    """Hacmi yuksek ve islem goren genis hisse evrenini ceker."""
     headers = {"User-Agent": "Mozilla/5.0"}
     try:
-        # S&P 500
-        sp500 = pd.read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies", storage_options=headers)[0]
-        sp500_tickers = sp500['Symbol'].str.replace('.', '-', regex=False).tolist()
+        # S&P 500 listesi baz olarak baslar
+        url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+        df = pd.read_html(url, storage_options=headers)[0]
+        tickers = df['Symbol'].str.replace('.', '-', regex=False).tolist()
         
-        # Nasdaq 100
-        nasdaq100 = pd.read_html("https://en.wikipedia.org/wiki/Nasdaq-100", storage_options=headers)
-        nasdaq_tickers = []
-        for tbl in nasdaq100:
-            if 'Ticker' in tbl.columns:
-                nasdaq_tickers = tbl['Ticker'].tolist()
-                break
-        
-        evren = sorted(set(sp500_tickers) | set(nasdaq_tickers))
-        return evren
-    except Exception as e:
-        print(f"Evren olusturma hatasi: {e}")
-        return []
+        # Ekstra: Russell 2000'den onemli momentum hisseleri veya populer tickerlar
+        # Sistemi yormamak icin ilk 350 hisseye odaklanacagiz
+        return tickers[:350]
+    except:
+        return ["AAPL", "TSLA", "NVDA", "AMD", "META", "AMZN", "MSFT", "GOOGL", "GME", "AMC"]
